6b90a5e91149717117e045820b30452ea89d6e4c
David <RedRock7777@gmail.com>
2017-10-12 20:34:44 -0700

    First commit

---
+++ 024-Images.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Images</title>
+    </head>
+    <body>
+    
+    <img src="Homer_Simpson_2006.png" width="100">
+    <p>Homer Simpson</p>
+    
+    <img src="https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png" width="100" align="left">
+    <p>Marge Simpson</p>
+    
+    </body>
---
+++ index.php
@@ -0,0 +1,64 @@
+
+<!doctype html>
+<html>
+<head>
+    <title>Hello World! Have a great day!</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+</head>
+
+    <body>
+        <iframe width="854" height="480" src="https://www.youtube.com/embed/RtU_mdL2vBM" frameborder="0" allowfullscreen align="right"></iframe>
+        
+        <h1>Hello World! Have a great day!</h1>
+        <h2>A few facts about the world we live in</h2>
+        <img src="338px-The_Earth_seen_from_Apollo_17.jpg" width="400px">
+        <p>Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.</p>
+        <hr>
+        <h3>About the earth</h3>
+        <ul>
+            <li>Equatorial Diameter:	12,756 km</li>
+            <li>Mass:	5.97 x 10^24 kg</li>
+            <li>Notable Moons:	<a href="http://space-facts.com/the-moon/">The Moon</a></li>
+        </ul>
+        <hr>
+        <h3>Top 3 facts about the earth</h3>
+        <ol>
+            <li>The Earth’s rotation is gradually slowing</li>
+            <li>The Earth was once believed to be the centre of the universe</li>
+            <li>Earth has a powerful magnetic field</li>
+        </ol>
+        <hr>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Radius</th>
+                    <th>Average <br>Temperature</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Mars</td>
+                    <td>3,396km</td>
+                    <td>-46&deg;</td>
+                
+                </tr>
+                <tr>
+                    <td>Venus</td>
+                    <td>6,052km</td>
+                    <td>462&deg;</td>
+                
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <form action="http://space-facts.com/earth/">
+        <p>Want to know more?</p>
+        <p><input type="submit" value="Click Here!"></p>
+        </form>
+    </body>
+</html>
